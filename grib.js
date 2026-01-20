f041125e6fbd4abab6e80c4aed5ede21ecb2899d
rigault <49497017+rigault@users.noreply.github.com>
2026-01-20 15:51:44 +0100

    Add files via upload

--- grib.js
+++ grib.js
@@ -10,56 +10,6 @@ window.dataGrib = dataGrib;
 window.gribLimits = gribLimits;
 
 /**
- * Condense list of time stamps in concise way 
- * @param {number[]} timeStamps
- * @returns {string}
- */
-function condenseTimeStamps (timeStamps) {
-   let result = [];
-   if (!timeStamps || timeStamps.length === 0) return "[]";
-   if (timeStamps.length < 5) {
-      for (let i = 0; i < timeStamps.length; i++) {
-         result.push (timeStamps [i]);
-      }
-      return "[" + result.join(", ") + "]";
-   }
-
-   let start = timeStamps[0];
-   let prev = start;
-   let timeStep = null;
-   let diff = null;
-   let afterStart = timeStamps [1];
-   for (let i = 1; i < timeStamps.length; i++) {
-      diff = timeStamps[i] - prev;
-
-      if (timeStep === null) {
-         timeStep = diff; // Initialize first time step
-      }
-
-      if (diff !== timeStep) {
-         // New sequence found
-         if (prev !== start) {
-            result.push(start + (prev !== start + timeStep ? ", " + afterStart + ".." + prev : ""));
-            afterStart = start + diff;
-         } else {
-            result.push(start);
-         }
-         start = timeStamps[i];
-         timeStep = diff;
-      }
-
-      prev = timeStamps[i];
-   }
-   afterStart = start + diff;
-
-   // Add last segment
-   if (prev !== start) result.push(start + ", " + afterStart + ".." + prev);
-   else result.push(start);
-
-   return "[" + result.join(", ") + "]";
-}
-
-/**
  * Fetches and displays metadata of a GRIB file (wind or current) from the server.
  *
  * This function requests GRIB metadata using {@link gribMetaAndLoad} with
